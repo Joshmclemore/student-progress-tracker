efba64f7c509420fec851312481a787a53d7c2f4
Joshmclemore <josh_mclemore@yahoo.com>
2022-05-15 17:47:23 -0500

    database is successfully  migrating

--- knexfile.js
+++ knexfile.js
@@ -9,10 +9,10 @@ const sharedConfig = {
 module.exports = {
     development: {
         ...sharedConfig,
-        connection: { filename: './data/cook_book.db3'},
+        connection: { filename: './data/student_progress.db3'},
     },
     testing: {
         ...sharedConfig,
-        connection: { filename: './data/cook_book.test.db3'},
+        connection: { filename: './data/student_progress.test.db3'},
     }
 }
