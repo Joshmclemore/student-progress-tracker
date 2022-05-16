d11c7180292cbbdc092349ae807df6e6db840508
Joshmclemore <josh_mclemore@yahoo.com>
2022-05-16 15:09:06 -0500

    added get/id route

--- api/students/students-model.js
+++ api/students/students-model.js
@@ -1,13 +1,9 @@
-// const db = require('../../data/db-config')
+const db = require('../../data/db-config')
 
-// async function getRecipeByID(student_id) {
-//     const recipeRows = await db('recipes as r')
-//         .where('recipe_id', recipe_id)
-//     return recipeRows
-// }
-
-function getStudentByID(student_id) {
-    return Promise.resolve(`this should return student with id:${student_id}`)
+async function getStudentByID(student_id) {
+    const studentRows = await db('students as s')
+        .where('student_id', student_id)
+    return studentRows
 }
 
 module.exports = { getStudentByID }
